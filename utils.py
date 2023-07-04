72af92ce38aece202d32c00b2a857ba28814852e
Iliya Malecki <iliyamalecki@gmail.com>
2023-07-05 00:17:01 +0200

    first commit

---
+++ utils.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+from tensorflow import keras
+import pandas as pd
+
+def plot_latent(decoder, scale, nonlinear=False):
+  # display a n * n 2D manifold of images
+  n = 25
+  img_dim = 28
+  if nonlinear:
+    scale = np.sqrt(scale)
+  figsize = 15
+  figure = np.zeros((img_dim * n, img_dim * n))
+  # linearly spaced coordinates corresponding to the 2D plot
+  # of images classes in the latent space
+  grid_x = np.linspace(-scale, scale, n)
+  grid_y = np.linspace(-scale, scale, n)[::-1]
+  if nonlinear:
+    grid_x = np.square(np.abs(grid_x))*np.sign(grid_x)
+    grid_y = np.square(np.abs(grid_y))*np.sign(grid_y)
+
+  for i, yi in enumerate(grid_y):
+    for j, xi in enumerate(grid_x):
+      z_sample = np.array([[xi, yi]])
+      x_decoded = decoder(z_sample, training=False).numpy()
+      images = x_decoded[0].reshape(img_dim, img_dim)
+      figure[
+        i * img_dim : (i + 1) * img_dim,
+        j * img_dim : (j + 1) * img_dim,
+      ] = images
+
+  plt.figure(figsize =(figsize, figsize))
+  start_range = img_dim // 2
+  end_range = n * img_dim + start_range
+  pixel_range = np.arange(start_range, end_range, img_dim)
+  sample_range_x = np.round(grid_x, 1)
+  sample_range_y = np.round(grid_y, 1)
+  plt.xticks(pixel_range, sample_range_x)
+  plt.yticks(pixel_range, sample_range_y)
+  plt.xlabel("z[0]")
+  plt.ylabel("z[1]")
+  plt.imshow(figure, cmap ="Greys_r")
+  plt.show()
+
+
+def evaluate(model, x_test, y_test):
+    intermediate_layer_model = keras.Model(inputs=model.encoder.input, outputs=model.encoder.get_layer('z_log_var').output)
+    vars = np.exp(0.5*intermediate_layer_model.predict(x_test[[0],:,:,:]))
+    print(f'{vars = }')
+    pred_means, pred_vars = model.encoder.predict(x_test)
+    preddf = pd.DataFrame(pred_means, columns=['x','y']).assign(label=y_test)
+    px.scatter(
+        preddf,
+        x='x',
+        y='y',
+        color='label',
+        width=600,
+        height=600,
+        opacity=0.7
+    ).update_traces(marker_size=3).show()
+    px.imshow(
+        np.stack([
+            x_test[28, ..., 0],
+            model.predict(x_test[[28]])[0, ..., 0]
+            ]),
+        facet_col=0
+    ).show()
+
+    plot_latent(
+        model.decoder,
+        preddf[['x','y']].abs().quantile(0.9).max()
+    )
---
+++ vaes.py
@@ -0,0 +1,120 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from keras.engine import data_adapter
+import plotly.express as px
+from plotly.subplots import make_subplots
+L = keras.layers
+
+
+class Sampling(L.Layer):
+
+	def call(self, inputs):
+		z_mean, z_log_var = inputs
+		epsilon = tf.keras.backend.random_normal(shape=tf.shape(z_mean))
+		return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+
+
+def decaying(length, initial, final, slope):
+    def weight_impl(iterations):
+        sigmoid_shape = 1 + tf.exp(
+            slope * (iterations - length)
+        )
+        return initial + (final - initial) / sigmoid_shape
+
+    return weight_impl
+
+
+
+
+
+class VAE(keras.Model):
+    def __init__(
+        self,
+        shape: tuple[int],
+        encoding_body: keras.Sequential,
+        decoding_body: keras.Sequential,
+        classify=True,
+        decay_function=None,
+        *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+
+        if classify and decay_function is None:
+            raise ValueError()
+        self.decay_function = decay_function
+
+        input = L.Input(shape)
+        encoded = encoding_body(input)
+        z_mean = L.Dense(2, name='z_mean')(encoded)
+        z_log_var = L.Dense(2, name='z_log_var')(encoded)
+
+
+        if classify:
+            self.classifier = keras.Sequential([
+                L.InputLayer((2,)),
+                L.Dense(10, activation='leaky_relu', name='classifier1'),
+                L.Dense(10, activation='leaky_relu', name='classifier2'),
+                L.Dense(10, activation='softmax', name='classifier3'),
+            ])
+        else:
+            self.classifier = None
+
+        self.encoder = keras.Model(input, [z_mean, z_log_var])
+        self.decoder = keras.Sequential([
+            L.InputLayer((2,)),
+            decoding_body
+        ])
+
+    def call(self, inputs, training=None, mask=None):
+        if training:
+            m, v = self.encoder(inputs, training=training)
+            sample = Sampling()([m,v])
+            return self.decoder(sample, training=training)
+        else:
+            m, v = self.encoder(inputs, training=training)
+            return self.decoder(m, training=training)
+
+    def train_step(self, data):
+        x, y, sample_weight = data_adapter.unpack_x_y_sample_weight(data)
+
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var = self.encoder(x, training=True)
+            sampled = Sampling()([z_mean, z_log_var])
+            decoded = self.decoder(sampled, training=True)
+            reconstruction_loss = tf.reduce_mean((x - decoded)**2) * 28 * 28
+            kl_divergence_loss = -0.5 * tf.reduce_mean(1 + z_log_var - tf.abs(z_mean) - tf.exp(z_log_var))
+            if self.classifier is not None:
+                weight = self.decay_function(tf.cast(self.optimizer.iterations, 'float32'))
+                pred = self.classifier(sampled)
+                classification_loss = tf.reduce_mean(
+                    keras.losses.sparse_categorical_crossentropy(
+                        y,
+                        pred
+                    )
+                )
+                loss = reconstruction_loss + kl_divergence_loss + classification_loss * weight
+                metrics = {
+                    'loss':loss,
+                    'reconstruction_loss':reconstruction_loss,
+                    'kl_divergence_loss':kl_divergence_loss,
+                    'classification_accuracy': tf.reduce_mean(keras.metrics.sparse_categorical_accuracy(y, pred)),
+                    'classification_loss':classification_loss,
+                    'classification_weight':weight,
+                }
+
+            else:
+                loss = reconstruction_loss + kl_divergence_loss
+                metrics = {
+                    'loss':loss,
+                    'reconstruction_loss':reconstruction_loss,
+                    'kl_divergence_loss':kl_divergence_loss,
+                }
+
+
+        self._validate_target_and_loss(y, loss)
+        self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
+        return metrics
